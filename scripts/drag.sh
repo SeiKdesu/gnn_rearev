dc0ba45b4d7916b57ee96ab990b3166b5c19ea18
SeiKdesu <g2581429@tcu.ac.jp>
2025-12-07 17:59:26 +0900

    shの更新

--- scripts/drag.sh
+++ scripts/drag.sh
@@ -1,23 +1,22 @@
 # 事前学習
 python main.py ReaRev \
-  --data_folder <data_dir> \
-  --experiment_name pretrain-rearev \
-  --num_epoch 10 \
-  --batch_size 16 \
+  --data_folder data/webqsp/ \
+  --experiment_name pretrain-rearev-2 \
+  --num_epoch 100 \
+  --batch_size 8 \
   --lr 5e-5 \
-  --drag_temperature 0.5
 
 
 # ファインチューニング
-python main.py DReaRev \
-  --data_folder <data_dir> \
-  --experiment_name drag-joint \
-  --num_epoch 18 \
-  --batch_size 16 \
-  --lr 5e-5 \
-  --drag_temperature 0.5 \
-  --lambda_sel 1.0 \
-  --selector_sparsity_weight 0.1 \
-  --selector_sparsity_target 0.2 \
-  --selector_entropy_weight 0.01 \
-  --load_experiment pretrain-rearev-final.ckpt
+# python main.py DReaRev \
+#   --data_folder data/webqsp/ \
+#   --experiment_name drag-joint \
+#   --num_epoch 18 \
+#   --batch_size 4 \
+#   --lr 5e-5 \
+#   --drag_temperature 0.5 \
+#   --lambda_sel 1.0 \
+#   --selector_sparsity_weight 0.1 \
+#   --selector_sparsity_target 0.2 \
+#   --selector_entropy_weight 0.01 \
+#   --load_experiment pretrain-rearev-final.ckpt
