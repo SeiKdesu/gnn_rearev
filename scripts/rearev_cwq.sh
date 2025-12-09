3e4d025d47d50243a28730173b5e4187f3ceca62
Your Namegit <you@example.com>
2025-12-09 16:06:12 +0900

    shの変更

--- scripts/rearev_cwq.sh
+++ scripts/rearev_cwq.sh
@@ -16,16 +16,4 @@
 
 # python main.py ReaRev --entity_dim 100 --num_epoch 20 --batch_size 4 --eval_every 2 --lm relbert --num_iter 2 --num_ins 3   --name cwq --experiment_name dim_100 --data_folder data/CWQ/ --warmup_epoch 80
 
-python main.py ReaRev --entity_dim 50 --num_epoch 100 --batch_size 8 --eval_every 2 --load_ckpt_file dim_50-29.ckpt --lm relbert --num_iter 2 --num_ins 3   --name cwq --experiment_name dim_50 --data_folder data/CWQ/ --warmup_epoch 80
-
-# python main.py ReaRev --entity_dim 150 --num_epoch 20 --batch_size 6 --eval_every 2 --lm relbert --num_iter 2 --num_ins 3   --name cwq --experiment_name dim_150 --data_folder data/CWQ/ --warmup_epoch 80
-
-# python main.py ReaRev --entity_dim 200 --num_epoch 20 --batch_size 4 --eval_every 2 --lm relbert --num_iter 2 --num_ins 3  --name cwq --experiment_name dim_200 --data_folder data/CWQ/ --warmup_epoch 80
-
-# python main.py ReaRev --entity_dim 250 --num_epoch 20 --batch_size 3 --eval_every 2 --lm relbert --num_iter 2 --num_ins 3   --name cwq --experiment_name dim_250 --data_folder data/CWQ/ --warmup_epoch 80
-
-# python main.py ReaRev --entity_dim 300 --num_epoch 20 --batch_size 1 --eval_every 2 --lm relbert --num_iter 2 --num_ins 3  --name cwq --experiment_name dim_300 --data_folder data/CWQ/ --warmup_epoch 80
-
-# python main.py ReaRev --entity_dim 350 --num_epoch 20 --batch_size 1 --eval_every 2 --lm relbert --num_iter 2 --num_ins 3  --name cwq --experiment_name dim_350 --data_folder data/CWQ/ --warmup_epoch 80
-
-# python main.py ReaRev --entity_dim 400 --num_epoch 20 --batch_size 1 --eval_every 2 --lm relbert --num_iter 2 --num_ins 3  --name cwq --experiment_name dim_400 --data_folder data/CWQ/ --warmup_epoch 80
+WANDB_API_KEY="66a8159cb49efe62675286b7216aecb7360a03a3" python main.py ReaRev --entity_dim 50 --num_epoch 100 --batch_size 8 --eval_every 2 --lm relbert --num_iter 2 --num_ins 3   --name webqsp --experiment_name dropout-0.2 --data_folder data/webqsp/ --warmup_epoch 80
