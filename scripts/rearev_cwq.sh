f5a2ff3f7f4161bda1a202b679689ea1f3ff381f
SeiKdesu <g2581429@tcu.acjp>
2026-01-06 15:26:07 +0900

    shの修正

--- scripts/rearev_cwq.sh
+++ scripts/rearev_cwq.sh
@@ -19,7 +19,7 @@
 # WANDB_API_KEY="66a8159cb49efe62675286b7216aecb7360a03a3" python main.py ReaRev --entity_dim 50 --num_epoch 100 --batch_size 8 --eval_every 2  --lm relbert --num_iter 2 --num_ins 3   --name cwq --experiment_name original_freebaseQA_dim_50 --data_folder data/SimpleQA/ --warmup_epoch 80
 WANDB_API_KEY="66a8159cb49efe62675286b7216aecb7360a03a3" python main.py ReaRev --entity_dim 50 --batch_size 8 --eval_every 2 \
   --lm relbert --num_iter 2 --num_ins 3 --num_gnn 3 --name cwq \
-  --data_folder data/CWQ_freebase/ --is_eval --load_experiment <ckpt> \
+  --data_folder data/CWQ/ 
   --enable_denoise true --encoder_type tfidf \
   --topM_strict 10 --topK_strict 50 --gamma_strict 0.2 \
   --topM_loose 25 --topK_loose 150 --gamma_loose 0.05 \
