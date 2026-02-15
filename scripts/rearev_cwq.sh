c3a26b4b54c335e0f3ae1292f871595431fa6e47
otkcsl <jiejidazhong68@gmail.com>
2026-02-15 13:16:55 +0900

    remote branch

--- scripts/rearev_cwq.sh
+++ scripts/rearev_cwq.sh
@@ -10,6 +10,7 @@
 # --experiment_name prn_cwq-rearev-lmsr  --data_folder data/CWQ/ --num_epoch 100 #--warmup_epoch 80
 
 
+
 ###Evaluate CWQ
 # python main.py ReaRev --entity_dim 50 --num_epoch 100 --batch_size 8 --eval_every 2 --data_folder data/CWQ/ --lm sbert --num_iter 2 --num_ins 3 --num_gnn 3 --relation_word_emb True --load_experiment ReaRev_CWQ.ckpt --is_eval --name cwq
 
