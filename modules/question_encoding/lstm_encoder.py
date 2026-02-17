7339f9a0263e74ee6309d79f60bd057c926d42a3
SeiKdesu <g2581429@tcu.acjp>
2026-02-17 09:44:44 +0900

    error

--- modules/question_encoding/lstm_encoder.py
+++ modules/question_encoding/lstm_encoder.py
@@ -16,6 +16,8 @@ class LSTMInstruction(BaseInstruction):
 
         self.word_embedding = word_embedding
         self.num_word = num_word
+        self.pad_val = num_word
+        self.question_emb = nn.Identity()
         self.encoder_def()
         entity_dim = self.entity_dim
         self.cq_linear = nn.Linear(in_features=4 * entity_dim, out_features=entity_dim)
